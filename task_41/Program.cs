72f7daea759fba190c9c8c11c2e21141b6070249
Your Name <you@example.com>
2022-10-06 21:52:27 +0300

    Homework_6

---
+++ task_41/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+
+using static System.Console;
+Clear();
+
+WriteLine("введите длину массива: ");
+int M = int.Parse(ReadLine()!);
+int[] array = GetArray(M);
+int res=0;
+for (int i = 0; i < M; i++)
+{
+    if (array[i]>0)
+    {
+        res = res +1;
+    }
+}
+WriteLine($"[{String.Join(", ", array)}]-> количество чисел больше 0 = {res}");
+
+
+
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        WriteLine($"Введите {i+1} числo: ");
+        int num = int.Parse(ReadLine()!);
+        result[i] = num;
+    }
+    return result;
+}
+
+
---
+++ task_43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// y = k1 * x + b1 
+// y = k2 * x + b2
+
+
+
+
+using static System.Console;
+Clear();
+WriteLine("введите значеник b1 ");
+double b1 = Convert.ToDouble(ReadLine()!);
+WriteLine("введите значеник b2 ");
+double b2 = Convert.ToDouble(ReadLine()!);
+WriteLine("введите значеник k1 ");
+double k1 = Convert.ToDouble (ReadLine()!);
+WriteLine("введите значеник k2 ");
+double k2 = Convert.ToDouble(ReadLine()!);
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+
+WriteLine($"Точка пересечения {x} {y}");
+
+
